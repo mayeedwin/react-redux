c90f4ce5595075ee1252829e86367d1de991850e
Maye Edwin <mayedwine1@gmail.com>
2022-03-05 14:10:16 +0300

    Rename

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
 
 import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
+import App from "./appx";
 import { store } from "./redux";
-import App from "./App";
-
 test("renders learn react link", () => {
   const { getByText } = render(
     <Provider store={store}>
